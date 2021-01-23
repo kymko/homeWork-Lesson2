a86db5739fbb2c20c61d7d9eaa89b6bb8301e2a8
kymko <Kymkobek@gmail.com>
2021-01-24 02:15:06 +0300

    изменил метод

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,15 +3,13 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println(ageAndDegree(10,40));
-        System.out.println(ageAndDegree(25,29));
-        System.out.println(ageAndDegree(30,35));
-        System.out.println(ageAndDegree(19,20));
-        System.out.println(ageAndDegree(46,30));
+        System.out.println(ageAndDegree(25,24));
+
 
     }
 
-    public static int ageAndDegree(int age, int degree){
+    public static String ageAndDegree(int age, int degree){
+        String result = "";
         if (age > 20 && age < 46 && degree > -20 && degree < 30)
             System.out.println("Можно идти гулять");
         else if (age < 20 && degree < 28)
@@ -19,6 +17,6 @@ public class Main {
         else if (age > 45 && degree > -10 && degree < 25)
             System.out.println("Можно идти гулять");
         else System.out.println("Оставайтесь дома");
-        return age;
+        return result;
     }
 }
