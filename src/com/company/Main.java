91d3382e8acbe8466c7614c679bbc77767cc615a
kymko <Kymkobek@gmail.com>
2021-01-26 16:44:16 +0300

    изменил метод Добавил дз насообразительность

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,15 +1,17 @@
 package com.company;
 
+import java.util.Random;
+
 public class Main {
 
     private static String home;
 
     public static void main(String[] args) {
-        System.out.println(ageAndDegree(50, -30));
-        System.out.println(ageAndDegree(12,20));
-        System.out.println(ageAndDegree(25,28));
-        System.out.println(ageAndDegree(35,-40));
-        System.out.println(ageAndDegree(40,55));
+        System.out.println(ageAndDegree(generateRandomAge(), 21));
+        System.out.println(ageAndDegree(generateRandomAge(),20));
+        System.out.println(ageAndDegree(generateRandomAge(),28));
+        System.out.println(ageAndDegree(generateRandomAge(),-40));
+        System.out.println(ageAndDegree(generateRandomAge(),55));
 
 
 
@@ -18,9 +20,9 @@ public class Main {
     }
 
     public static  String ageAndDegree(int age, int degree){
-        if (age > 20 && age < 46 && degree > -20 && degree < 30){
+        if ((age > 20 && age < 46) && (degree > -20 && degree < 30)){
             return "Можно гулять!";
-        } else if (age < 20 && degree < 28){
+        } else if (age < 20 && degree >0 && degree < 28){
         return "Можно идти гулять";
         } else if (age > 45 && degree > -10 && degree < 25){
             return "Гуляй смело";
@@ -28,5 +30,11 @@ public class Main {
             String home = ("Оставайтесь дома");
             return home;}
 
+    }
+    public static int generateRandomAge(){
+        Random random=new Random();
+        int age;
+        age=random.nextInt(50);
+        return age;
     }
 }
